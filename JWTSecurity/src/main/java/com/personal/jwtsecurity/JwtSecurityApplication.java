c37f45beeb1c42b8e71eea1df2bd7bcbb300c4df
stasim101 <75426751+stasim101@users.noreply.github.com>
2020-12-20 20:18:14 +0530

    Adding annotation for EurekaDiscoveryClient

--- JWTSecurity/src/main/java/com/personal/jwtsecurity/JwtSecurityApplication.java
+++ JWTSecurity/src/main/java/com/personal/jwtsecurity/JwtSecurityApplication.java
@@ -3,6 +3,7 @@ package com.personal.jwtsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class JwtSecurityApplication {
 
