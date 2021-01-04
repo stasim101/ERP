07915a4b93634a11077ac64341fd27e30748c6e2
imran ansari <stasim101@gmail.com>
2021-01-04 12:35:52 +0530

    Adding new service

---
+++ FA/src/test/java/com/personal/fa/FaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
